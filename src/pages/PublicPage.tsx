3d8d05dc817e936f42aa6d57d1eabd8b241a5e16
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2023-12-18 15:30:43 +0100

    Update PublicPage.tsx

--- src/pages/PublicPage.tsx
+++ src/pages/PublicPage.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export function PublicPage() {
-  return <h1>Public Page</h1>;
+  return <>
+    <h1>Public Page</h1>
+    <Link to="/protected" >
+      Link to protected route
+    </Link>
+  </>;
 }
