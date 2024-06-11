a7076be68ccacfa11c681187703eb6c20c71c8bc
Dylan Decrulle <dylan.decrulle@outlook.fr>
2024-06-11 14:22:40 +0200

    accessibility page

--- src/i18n/types.ts
+++ src/i18n/types.ts
@@ -20,6 +20,7 @@ export type ComponentKey =
   | import('shared/components/Orchestrator/CustomPages/WelcomeModal').I18n
   | import('shared/components/Orchestrator/SequenceHeader').I18n
   | import('shared/components/Orchestrator/SurveyContainer').I18n
+  | import('pages/Accessibility/AccessibilityPage').I18n
 
 export type Translations<L extends Language> = GenericTranslations<
   ComponentKey,
--- src/pages/Accessibility/AccessibilityPage.tsx
+++ src/pages/Accessibility/AccessibilityPage.tsx
@@ -1,15 +1,22 @@
 import { Breadcrumb } from '@codegouvfr/react-dsfr/Breadcrumb'
+import { useTranslation } from 'i18n'
+import { declareComponentKeys } from 'i18nifty'
 import { Grid } from 'shared/components/Grid'
 
 export function AccessibilityPage() {
+  const { t } = useTranslation('AccessibilityPage')
   return (
     <Grid>
       <Breadcrumb
-        currentPageLabel="Accessibilité"
+        currentPageLabel={t('title')}
         homeLinkProps={{}}
         segments={[]}
       />
-      <h2>Accessibilité</h2>
+      <h2>{t('title')}</h2>
     </Grid>
   )
 }
+
+const { i18n } = declareComponentKeys<'title'>()('AccessibilityPage')
+
+export type I18n = typeof i18n
